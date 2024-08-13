4c3e008b5e1aab3401804e0eaeffc27ca57d7fc1
Daniel Villatoro <57822891+DanielVillatoro@users.noreply.github.com>
2024-08-13 06:56:25 -0600

    DTO implementations and use of arrays vs maps.

---
+++ springboot-webapp/src/main/java/com/daniel/curso/springboot/webapp/springboot_webapp/DTO/UserDto.java
@@ -0,0 +1,22 @@
+package com.daniel.curso.springboot.webapp.springboot_webapp.DTO;
+
+import com.daniel.curso.springboot.webapp.springboot_webapp.models.User;
+
+public class UserDto {
+    private String title;
+    private User user;
+    public String getTitle() {
+        return title;
+    }
+    public void setTitle(String title) {
+        this.title = title;
+    }
+    public User getUser() {
+        return user;
+    }
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    
+}
--- springboot-webapp/src/main/java/com/daniel/curso/springboot/webapp/springboot_webapp/controllers/UserRestController.java
+++ springboot-webapp/src/main/java/com/daniel/curso/springboot/webapp/springboot_webapp/controllers/UserRestController.java
@@ -1,11 +1,12 @@
 package com.daniel.curso.springboot.webapp.springboot_webapp.controllers;
 
-import java.util.HashMap;
-import java.util.Map;
+// import java.util.HashMap;
+// import java.util.Map;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.daniel.curso.springboot.webapp.springboot_webapp.DTO.UserDto;
 import com.daniel.curso.springboot.webapp.springboot_webapp.models.User;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,14 +18,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class UserRestController {
 
 
-    @GetMapping("/details")    
-    public Map<String,Object> details(){
-        User user=new User("Daniel","Villatoro");
-        Map<String, Object> body = new HashMap<>();
+    // @GetMapping("/details")    
+    // public Map<String,Object> details(){
+    //     User user=new User("Daniel","Villatoro");
+    //     Map<String, Object> body = new HashMap<>();
 
-        body.put("title","Hola Daniel");
-        body.put("user",user);
-        return body;
+    //     body.put("title","Hola Daniel");
+    //     body.put("user",user);
+    //     return body;
+    // }
+    
+    @GetMapping("/details")    
+    public UserDto details(){
+        User user=new User("Daniel","Villatoro");
+        UserDto userDto = new UserDto();
+        userDto.setUser(user);
+        userDto.setTitle("Hola Daniel");
+        return userDto;
     }
 
 }
